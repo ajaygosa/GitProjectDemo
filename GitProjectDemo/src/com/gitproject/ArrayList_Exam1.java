eaf28e8c57697eec82e4404672794a93ccf08b7d
Ajay Gosavi <ajaygosavi001@gmail.com>
2022-11-15 13:12:42 +0530

    code for arrayList

---
+++ GitProjectDemo/src/com/gitproject/ArrayList_Exam1.java
@@ -0,0 +1,19 @@
+package com.gitproject;
+
+import java.util.ArrayList;
+
+public class ArrayList_Exam1 {
+
+	public static void main(String[] args) {
+
+		ArrayList al = new ArrayList();
+		al.add("jagruti");
+		al.add("Anil");
+		
+		for(Object str :al)
+		{
+			System.out.println(str);
+		}
+	}
+
+}
